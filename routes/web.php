0879f45cd017910f016211f5bed67bc8df07764c
GabrielOB <gabriel.oliveira@aluno.fapce.edu.br>
2019-09-25 21:29:40 -0300

    Atualizando rota listar serviço

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ $router->post('/logout', 'UsuarioController@usuarioLogout');
 $router->group(['prefix' => 'servico'], function () use($router){
     $router->post('/cadastrar', 'ServicoController@cadastrarServico');
 
-    $router->get('/{id}', 'ServicoController@mostrarServico');
+    $router->get('/listar', 'ServicoController@mostrarServico');
 
     $router->put('/{id}/atualizar', 'ServicoController@atualizarServico');
 
