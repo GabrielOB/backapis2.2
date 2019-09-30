c675a41dcb002096863a9b7b326f7782aa147324
GabrielOB <gabriel.oliveira@aluno.fapce.edu.br>
2019-09-30 17:39:47 -0300

    Criada migração  tabela avaliações

---
+++ database/migrations/2019_09_30_203259_create_avaliacoes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAvaliacoesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('avaliacoes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_cliente');
+            $table->unsignedBigInteger('id_prestador');
+            $table->Integer('nota');
+            $table->string('conteudo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('avaliacoes');
+    }
+}
