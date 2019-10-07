88788235eccb048cff82042132c6ee3278f2dcea
GabrielOB <gabriel.oliveira@aluno.fapce.edu.br>
2019-10-07 15:15:40 -0300

    Nova migrate para table avaliacaos

--- database/migrations/2019_10_07_181336_create_avaliacaos_table.php
+++ database/migrations/2019_10_07_181336_create_avaliacaos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAvaliacoesTable extends Migration
+class CreateAvaliacaosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAvaliacoesTable extends Migration
      */
     public function up()
     {
-        Schema::create('avaliacoes', function (Blueprint $table) {
+        Schema::create('avaliacaos', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('id_cliente');
             $table->unsignedBigInteger('id_prestador');
