62ff3f9bd11c5ddb6fd95778b28f37a68f457626
antonylrds <42950733+antonylrds@users.noreply.github.com>
2019-11-12 19:52:20 -0300

    MIgration de chat

---
+++ database/migrations/2019_11_12_224228_create_chat_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChatTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('id_provider');
+            $table->unsignedInteger('id_client');
+            $table->timestamps();
+
+            $table->foreign('id_client')->references('id')->on('usuarios');
+            $table->foreign('id_provider')->references('id')->on('usuarios');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat');
+    }
+}
