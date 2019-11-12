e44b41c9c000fc9e77017e1bd8c971cdfd3423c5
antonylrds <42950733+antonylrds@users.noreply.github.com>
2019-11-12 19:53:08 -0300

    Controller de chat

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Chat;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\JWTAuth;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller{
+
+}
+
