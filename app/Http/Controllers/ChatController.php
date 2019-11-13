cab4a781fc93ff539a10733408de8484f9a65be2
antonylrds <42950733+antonylrds@users.noreply.github.com>
2019-11-12 22:57:14 -0300

    Função store incompleta

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -8,6 +8,19 @@ use Tymon\JWTAuth\JWTAuth;
 use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller{
+    protected $jwt;
 
+    public function __construct(JWTAuth $jwt)
+    {
+        $this->jwt = $jwt;
+        $this->middleware('auth:api');
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'id_provider' => 'required',
+            'id_client' => 'required',
+        ]);
+    }
 }
 
